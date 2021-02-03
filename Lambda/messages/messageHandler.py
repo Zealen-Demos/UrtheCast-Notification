a27cec2a088a37933cc3ab4cecb7e496f76c0de9
Dylan Jack <fall_back_up@hotmail.com>
2021-02-03 05:50:57 -0800

    Text functionality

--- Lambda/messages/messageHandler.py
+++ Lambda/messages/messageHandler.py
@@ -1,8 +1,38 @@
 import json
+import logging
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3 = boto3.client('s3')
+sns = boto3.client('sns')
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+	for record in event.get('Records', []):
+		bucket = record['s3']['bucket']['name']
+		key = record['s3']['object']['key']
+		data = json.loads(json.dumps(record))
+		
+		logger.info(data)
+		
+		if 'Message' not in data or data['Message'] == "":
+			logger.info("No message included in file")
+		else:
+			try:
+				logger.info("Processing file from: ")
+				sendSMS(data['Message'], data['Phone'])
+
+			except Exception as e:
+				logger.exception("Error processing file: ")
+
+def sendEmail(message, address):
+	pass
+
+def sendSMS(message, number):
+	sns.publish(PhoneNumber = number, TextMessage = message)
+	pass
+
+def writeDB(record, db):
+	pass
